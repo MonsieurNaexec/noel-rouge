af32c47966c8cff2b94f30469be4f10bf5737e68
MonsieurNaexec <43294047+MonsieurNaexec@users.noreply.github.com>
2021-12-18 16:21:43 +0100

    We wish you a merry christmas

--- main.ts
+++ main.ts
@@ -1,17 +1,75 @@
 const songs = {
     "We wish you a merry christmas": [
         { note: Note.C, length: 1 },
-        { note: Note.F, length: 0.75 },
-        { note: 0, length: 0.25 },
-        { note: Note.F, length: 0.5 },
-        { note: Note.G, length: 0.5 },
+        { note: Note.F, length: 1 },
+        { note: Note.F, length: .5 },
+        { note: Note.G, length: .5 },
+        { note: Note.F, length: .5 },
+        { note: Note.E, length: .5 },
+        { note: Note.D, length: 1 },
+        { note: Note.D, length: 1 },
+        { note: Note.D, length: 1 },
+        { note: Note.G, length: 1 },
+        { note: Note.G, length: .5 },
+        { note: Note.A, length: .5 },
+        { note: Note.G, length: .5 },
+        { note: Note.F, length: .5 },
+        { note: Note.E, length: 1 },
+        { note: Note.E, length: 1 },
+        { note: Note.C, length: 1 }, 
+        { note: Note.A, length: 1 },
+        { note: Note.A, length: .5 },
+        { note: Note.Bb, length: .5 },
+        { note: Note.A, length: .5 },
+        { note: Note.G, length: .5 },
+        { note: Note.F, length: 1 },
+        { note: Note.D, length: 1 },
+        { note: Note.C, length: .5 },
+        { note: Note.C, length: .5 },
+        { note: Note.D, length: 1 },
+        { note: Note.G, length: 1 },
+        { note: Note.E, length: 1 },
+        { note: Note.F, length: 2 },
+        { note: Note.C, length: 1 },
+        { note: Note.F, length: 1 },
+        { note: Note.F, length: 1 },
+        { note: Note.F, length: 1 },
+        { note: Note.E, length: 2 },
+        { note: Note.E, length: .5 },
+        { note: Note.E, length: .5 },
+        { note: Note.F, length: 1 },
+        { note: Note.E, length: 1 },
+        { note: Note.D, length: 1 },
+        { note: Note.C, length: 2 },
+        { note: Note.G, length: 1 },
+        { note: Note.A, length: 1 },
+        { note: Note.G, length: 1 },
+        { note: Note.F, length: .5 },
+        { note: Note.F, length: .5 },
+        { note: Note.C5, length: 1 },
+        { note: Note.C, length: 1 },
+        { note: Note.C, length: .5 },
+        { note: Note.C, length: .5 },
+        { note: Note.D, length: 1 },
+        { note: Note.G, length: 1 },
+        { note: Note.E, length: 1 },
+        { note: Note.F, length: 2 },
     ]
 }
 
 input.onButtonPressed(Button.A, function () {
     const song = songs["We wish you a merry christmas"]
-    music.setTempo(120)
-    song.forEach(n => n.note ? music.playTone(262, music.beat(BeatFraction.Whole)) : music.rest(music.beat(BeatFraction.Whole)))
-    
-    
+    const beatLength = 60000 / 120
+    song.forEach(n => {
+        if(n.note){
+            music.ringTone(n.note)
+            control.waitMicros(beatLength * n.length * 900)
+            music.setVolume(127)
+            control.waitMicros(beatLength * n.length * 100)
+            music.setVolume(255)
+        }else{
+            music.rest(beatLength * n.length)
+        }
+    })
+    music.stopAllSounds()    
 })
